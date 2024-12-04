092b744fc312bb941938ddb7fca810e1ea50ba30
MiKatina <katrina.mihailova@grobiinasskola.lv>
2024-12-04 14:39:05 +0200

    3. komentēju

--- 03/main.py
+++ 03/main.py
@@ -1,11 +1,11 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]# saraksts ar cipariem 
 
-num = int(input("Choose a number: "))
+num = int(input("Choose a number: ")) # aprēķināšana ar veseliem skaitliem, izvēlās ciparu 
 
 new_list = []
 
 for i in a:
     if i < num:
-        new_list.append(i)
+        new_list.append(i) # pievieno 
 
 print(new_list)
