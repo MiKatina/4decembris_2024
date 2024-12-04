78e8d7c309a9753f542d5d230a0abb2d76788975
MiKatina <katrina.mihailova@grobiinasskola.lv>
2024-12-04 14:26:55 +0200

    komenteju 2. uzdevumu

--- 02/main.py
+++ 02/main.py
@@ -1,24 +1,24 @@
 """ Method 1 """
 
-num = int(input("Give me a number to check: "))
-check = int(input("Give me a number to divide by: "))
+num = int(input("Give me a number to check: ")) # prasa ievadīt skaitli
+check = int(input("Give me a number to divide by: ")) # ievadīt skaitli ar kuru tiks dalīts
 
 if num % 4 == 0:
-    print(num, "is a multiple of 4")
+    print(num, "is a multiple of 4") #  norāda ja tas reizinas ar 4 
 elif num % 2 == 0:
-    print(num, "is an even number")
+    print(num, "is an even number") # norāda ja ir pāra skaitlis
 else:
-    print(num, "is an odd number")
+    print(num, "is an odd number") # norāda ja ir nepāra skaitlis
 
-if num % check == 0:
-    print(num, "divides evenly by", check)
+if num % check == 0: #parbauda vai nav 0 
+    print(num, "divides evenly by", check) # norāda ja to ir iespējams dalīt
 else:
-    print(num, "doesn't divide evenly by", check)
+    print(num, "doesn't divide evenly by", check) #norāda ja to nav iespējams dalit ar uzrakstito skaitli
 
 """ Method 2 """
-num = int(input("Enter a number: "))
-mod = num % 2
-if mod > 0:
-    print("You picked an odd number.")
+num = int(input("Enter a number: ")) # ievadit skaitli
+mod = num % 2 # mob ir  skitlis kura atlikums ir 2
+if mod > 0: #  norāda ja mod ir mazāks par 0
+    print("You picked an odd number.") # norāda ja ir nepāra 
 else:
-    print("You picked an even number.")
+    print("You picked an even number.") # norāda ja ir pāraS
